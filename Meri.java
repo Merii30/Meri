3ee0deb818c4931f8687f502166e02260845e73b
Merii30 <71128714+Merii30@users.noreply.github.com>
2020-09-11 19:40:25 +0700

    Add files via upload

---
+++ Meri.java
@@ -0,0 +1,14 @@
+package Meri;
+
+public class Meri {
+    public static void main (String[]args){
+String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+System.out.println(alfabet.charAt(12));
+System.out.println(alfabet.charAt(4));
+System.out.println(alfabet.charAt(17));
+System.out.println(alfabet.charAt(8));
+
+
+}
+}
